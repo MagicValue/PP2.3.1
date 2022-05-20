205af210779617cdac4e0463bb91c315b16584a1
MagicValue <sanek1023jr12@yandex.by>
2022-05-20 15:38:18 +0300

    delete Transactional above select methods

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -8,35 +8,35 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserDao userDao;
 
-    @Transactional
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
     @Override
     public List<User> index() {
         return userDao.index();
     }
 
-    @Transactional
     @Override
     public User show(int id) {
         return userDao.show(id);
     }
 
-    @Transactional
     @Override
     public void save(User user) {
         userDao.save(user);
     }
 
-    @Transactional
     @Override
     public void update(int id, User user) {
         userDao.update(id, user);
     }
 
-    @Transactional
     @Override
     public void delete(int id) {
         userDao.delete(id);
